cfcda06a33a7dd499c7479a94d4e3fd3545e5f3c
bradygillam <bradygillam3468@gmail.com>
2025-03-30 00:47:08 -0300

    Fresh Start

--- Waves/Assets/Scripts/Global.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-
-public static class Global
-{
-    public static ArrayList ENEMIES = new ArrayList();
-    public static float playerCash = 100f;
-}
--- Waves/Assets/Scripts/Recruit_E.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Recruit_E : BasicUnit_Enemy
-{
-    protected override float speed { get; set; } = 0.5f;
-}
